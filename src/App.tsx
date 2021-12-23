891996b018e3559a88c144034169738908ffb12a
Don Jung <don.j_1116@outlook.com>
2021-12-23 17:45:50 +0900

    remove unused function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useState } from "react";
 import styled from "styled-components";
 // import WalletConnect from "@walletconnect/client";
 // import QRCodeModal from "@walletconnect/qrcode-modal";
