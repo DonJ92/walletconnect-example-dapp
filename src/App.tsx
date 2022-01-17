8f97735e08cabae0fafb9c73e1f50c0e5e7c9805
Don Jung <don.j_1116@outlook.com>
2022-01-17 22:33:42 +0900

    Add Sell Cancel

--- src/App.tsx
+++ src/App.tsx
@@ -776,6 +776,103 @@ class App extends React.Component<any, any> {
     }
   };
 
+  public testSellCancelTransaction = async () => {
+    const { address/*, chainId*/ } = this.state;
+
+    if (!this.state.connected) {
+      return;
+    }
+
+    // sell NFT token contract address
+    const sellToken = '0x0000000000000000000000000000000000001014';
+
+    // sell NFT token id
+    const sellTokenId = 2;
+
+    // from
+    const from = address;
+
+    // exchange contract address
+    const to = '0xDbF58aD64a728B42AEB9252E6dE5d48a1C478968';
+
+    // nonce
+    const _nonce = await apiGetAccountNonce(address, this.state.chainId);
+    const nonce = sanitizeHex(convertStringToHex(_nonce));
+
+    // gasPrice
+    const gasPrices = await apiGetGasPrices();
+    let _gasPrice = gasPrices.slow.price;
+    _gasPrice = 0;
+    const gasPrice = sanitizeHex(convertStringToHex(convertAmountToRawNumber(_gasPrice, 9)));
+
+    // value
+    const _value = 0;
+    const value = sanitizeHex(convertStringToHex(_value));
+
+    // data
+    // const web3 = new Web3(this.provider as unknown as AbstractProvider);
+    const web3 = new Web3(Web3.givenProvider);
+    const exchange = new web3.eth.Contract(ExchangeABI as AbiItem[], to);
+    const data = exchange.methods.cancelSell(sellToken, sellTokenId).encodeABI({
+      nonce: parseInt(nonce, 16),
+      from,
+      to,
+      value,
+      gasPrice,
+      gas: 0
+    });
+
+    const tx: TransactionConfig = {
+      nonce: parseInt(nonce, 16),
+      from,
+      to,
+      value,
+      data,
+      gasPrice,
+      gas: 0
+    };
+
+    try {
+      // open modal
+      this.toggleModal();
+
+      // toggle pending request indicator
+      this.setState({ pendingRequest: true });
+
+      // const web3 = new Web3(this.provider as unknown as AbstractProvider);
+      web3.eth.sendTransaction(tx)
+      .once('sending', (payload: any) => { console.log('sending') })
+      .once('sent', (payload: any) => { console.log('sent') })
+      .once('transactionHash', (hash: string) => { console.log(hash) })
+      .once('receipt', (receipt: any) => { console.log(receipt) })
+      .then((res: any) => {
+        console.log(res);
+
+        const formattedResult = {
+          method: "sell request",
+          txHash: res.transactionHash,
+          from: address,
+          to,
+        };
+  
+        // display result
+        this.setState({
+          // connector,
+          pendingRequest: false,
+          result: formattedResult || null,
+        });
+      })
+      .catch((err: any) => {
+        console.log(err);
+        this.setState({ /*connector, */pendingRequest: false, result: null });
+      })
+
+    } catch (error) {
+      console.error(error);
+      this.setState({ /*connector, */pendingRequest: false, result: null });
+    }
+  };
+
   public testSignMessage = async () => {
     const { address, chainId } = this.state;
 
@@ -941,6 +1038,10 @@ class App extends React.Component<any, any> {
 
                     <STestButton left onClick={this.testSellRequestTransaction}>
                       {"sellRequest_Transaction"}
+                    </STestButton>
+
+                    <STestButton left onClick={this.testSellCancelTransaction}>
+                      {"sellCancel_Transaction"}
                     </STestButton>
                   </STestButtonContainer>
                 </Column>
