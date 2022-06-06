2bf5196d53d00fa1b7c304ec570a40105b8c8314
DonJung <don.j_1116@outlook.com>
2022-06-06 13:23:48 +0900

    Update Ethereum API URL

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { IAssetData, IGasPrices, IParsedTx } from "./types";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://54.199.226.190:3002/",
+  baseURL: "https://ethereum-api.xyz",
   timeout: 30000, // 30 secs
   headers: {
     Accept: "application/json",
