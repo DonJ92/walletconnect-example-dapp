4f8e70128ca5ee790ea1b44584bdd573b85e85d8
DonJung <don.j_1116@outlook.com>
2022-06-09 11:41:26 +0900

    Update Exchange Contract Address

--- src/App.tsx
+++ src/App.tsx
@@ -164,7 +164,7 @@ const STestButton = styled(Button as any)`
 
 const NFT_contract = '0x0000000000000000000000000000000000001113';
 const PLT_contract = '0x0000000000000000000000000000000000000103';
-const MP_contract = '0x001d35e5787a33B67FA2DC2feF7536dF92AA3a3a';
+const MP_contract = '0x1463A3Adf12c2442446Eb73E2FAa77eE0d4407E1';
 const NFT_owner_address = "0x81c4ed66e83cceaf12d7a5807d91fe55207f0a4a";
 const TO_address_for_PLT = '0x983ab5ff5ff2cd4d0b7b5f67113bb8e41af1e663';
 const PLT_transfer_amount = 10000;
